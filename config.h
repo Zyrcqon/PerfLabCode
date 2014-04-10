650eb638282539c07a91475b05e77c1004b8ff65
Neil Patel <neil92@csa2.bu.edu>
2014-04-10 18:20:26 -0400

    Started Performance Lab

---
+++ config.h
@@ -0,0 +1,32 @@
+/*********************************************************
+ * config.h - Configuration data for the driver.c program.
+ *********************************************************/
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* 
+ * CPEs for the baseline (naive) version of the rotate function that
+ * was handed out to the students. Rd is the measured CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define R64    3.7
+#define R128   4.1
+#define R256   12.2
+#define R512   13.7
+#define R1024  26.0
+
+/* 
+ * CPEs for the baseline (naive) version of the smooth function that
+ * was handed out to the students. Sd is the measure CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define S32   218.3
+#define S64   219.1
+#define S128  221.0
+#define S256  222.9
+#define S512  223.2
+
+
+#endif /* _CONFIG_H_ */
